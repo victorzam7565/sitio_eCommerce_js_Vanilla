a5a83999d09cfa166cec24c749a4ff48eb309f6a
VictorZambrano <ingenieriainversazambrano@gmail.com>
2021-07-14 16:13:45 -0500

    carrito terminado

--- app.js
+++ app.js
@@ -8,6 +8,10 @@ const fragment = document.createDocumentFragment()
 let carrito = {}
 document.addEventListener('DOMContentLoaded',()=> {
   fetchData()
+  if (localStorage.getItem('carrito')){
+     carrito =JSON.parse(localStorage.getItem('carrito'))
+     pintarCarrito()
+  }
 })
 cards.addEventListener('click', e =>{
    addCarrito(e)
@@ -77,6 +81,8 @@ const pintarCarrito = ()=> {
    })
    items.appendChild(fragment)
     pintarFooter ()
+
+    localStorage.setItem('carrito', JSON.stringify(carrito))
     }
     const pintarFooter = () => { 
       footer.innerHTML = ''   
